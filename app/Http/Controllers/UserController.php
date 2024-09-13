01efd3ff09afe49c212041f6cb20e5ffbeadbaf5
bale <iqbalzfaadhihillah@gmail.com>
2024-09-13 18:53:44 +0700

    modified:   app/Http/Controllers/UserController.php 	new file:   app/Models/UserModel.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,22 +10,23 @@ class UserController extends Controller
 {
     public function index()
     {
-        // Tambah data user dengan Eloquent Model
-        /*$data = [
-            'username' => 'Customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
-        ];
-        UserModel::insert($data); // Tambahkan data ke tabel m_user*/
+        // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4,
+        // ];
+        // UserModel::insert( $data ); // tambahkan data ke tabel m_user
 
+        // tambah data user dengan Eloquent Model
         $data = [
             'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::where('username', 'Customer-1')->update($data);
+        UserModel::where('username', 'customer-1')->update($data); // update data user
 
-        // Coba akses model UserModel
-        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        // Coba askses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
